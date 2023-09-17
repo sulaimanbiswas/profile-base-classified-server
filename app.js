1e7c5e21156dbec186db144b0fe2797b4ab84f85
Sulaiman Biswas <sulaimanbiswasbd@gmail.com>
2023-09-18 01:42:24 +0600

    resetup

--- app.js
+++ app.js
@@ -7,13 +7,8 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-const userRouter = require("./routes/user.route");
-
-// Routes
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-
-app.use("/api/v1/user", userRouter);
 
 module.exports = app;
